224db5cae3d5e9f5013db28567f6f42a99316283
Morpa <andremorpa@gmail.com>
2022-07-20 21:56:45 +0100

    add styled components

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components';
+
+type ButtonProps = {
+  variant: 'primary' | 'secondary' | 'danger' | 'success';
+};
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+};
+
+export const ButtonWrapper = styled.button<ButtonProps>`
+  width: 100px;
+  height: 40px;
+
+  ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }}
+`;
