a5f4c8d3dd4250fb82e69c0d3d2e16089ad50efc
Morpa <andremorpa@gmail.com>
2022-07-21 20:05:51 +0100

    add router

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as Styles from './styles'
-
-type ButtonProps = {
-  variant?: 'primary' | 'secondary' | 'danger' | 'success'
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <Styles.ButtonWrapper variant={variant}>enviar</Styles.ButtonWrapper>
-}
