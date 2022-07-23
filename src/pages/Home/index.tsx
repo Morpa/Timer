46b834d97a2bd800ed174cae0334704ae032b381
Morpa <andremorpa@gmail.com>
2022-07-23 07:50:16 +0100

    input improvements

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,12 +8,30 @@ export function Home() {
       <S.Form>
         <S.FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <S.TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <S.MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
-          <S.TimerItem>minutos.</S.TimerItem>
+          <span>minutos.</span>
         </S.FormContainer>
 
         <S.CountdownContainer>
@@ -24,10 +42,10 @@ export function Home() {
           <S.TimerItem>0</S.TimerItem>
         </S.CountdownContainer>
 
-        <button type="submit">
+        <S.StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </S.StartCountdownButton>
       </S.Form>
     </S.Wrapper>
   )
